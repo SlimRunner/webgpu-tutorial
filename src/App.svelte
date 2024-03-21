1acca56428882a2034f41b6921d4cd03b4e7edf3
SlimRunner <dv.polygon@gmail.com>
2024-03-20 23:33:35 -0700

    Configure WebGPU

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,35 @@
 <script lang="ts">
-  // code for tutorial here
+  import { onMount } from "svelte";
+
+  onMount(async () => {
+    const canvas = document.querySelector("canvas");
+
+    if (!canvas) {
+      throw new Error("Canvas not present.");
+    }
+
+    if (!navigator.gpu) {
+      throw new Error("WebGPU not supported on this browser.");
+    }
+
+    const adapter = await navigator.gpu.requestAdapter();
+    if (!adapter) {
+      throw new Error("No appropriate GPUAdapter found.");
+    }
+
+    const device = await adapter.requestDevice();
+
+    const context = canvas.getContext("webgpu");
+    const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+    if (!context) {
+      throw new Error("Context failed to be initialized");
+    }
+
+    context.configure({
+      device: device,
+      format: canvasFormat
+    });
+  });
 </script>
 
 <main>

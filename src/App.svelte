2c3bae95c4e89ae91158789596624bb908f28fba
SlimRunner <dv.polygon@gmail.com>
2024-03-28 19:04:55 -0700

    Improve game loop

--- src/App.svelte
+++ src/App.svelte
@@ -178,7 +178,49 @@
     const UPDATE_INTERVAL = 200;
     let step = 0;
 
-    setInterval(() => updateGrid(context), UPDATE_INTERVAL);
+    // use a proper game loop
+    // reference: https://stackoverflow.com/a/19772220
+
+    class GameLoop {
+      fps: number;
+      fpsInterval: number;
+      drawCall: Function;
+      timeOffset: number;
+      frameOffset: number;
+      time: number;
+      timeInFrame: number;
+      timeDelta: number;
+
+      constructor(fps: number, drawCall: Function) {
+        this.timeOffset = Date.now();
+        this.frameOffset = 0;
+        this.fps = fps;
+        this.fpsInterval = 1000 / fps;
+        this.drawCall = drawCall;
+        this.time = 0;
+        this.timeInFrame = 0;
+        this.timeDelta = 0;
+      }
+
+      runLoop() {
+        requestAnimationFrame(() => this.runLoop());
+
+        const now = Date.now();
+        const newTime = now - this.timeOffset;
+        this.timeDelta = newTime - this.time;
+        this.time = newTime;
+        this.timeInFrame = now - this.frameOffset;
+
+        if (this.timeInFrame > this.fpsInterval) {
+          this.frameOffset = now - (this.timeInFrame % this.fpsInterval);
+
+          this.drawCall();
+        }
+      }
+    }
+
+    const game = new GameLoop(5, (): void => updateGrid(context));
+    game.runLoop();
     
     function updateGrid(context: GPUCanvasContext) {
       ++step;
